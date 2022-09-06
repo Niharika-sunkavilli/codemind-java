0ff2bf3e8a745fe8feff98488bc2bee8e7c2cc76
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-09-06 19:00:29 +0530

    import java.util.Scanner; class Main {     public static boolean isprime(int n)     {     int count=0;     if(n==1)     return false;     for(int i=2;i<=(int)Math.sqrt(n);i++)     {         if(n%i==0)         count++;     }     if(count==0)     return true;     else     return false;
        }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n;
            int arr[];
            n=sc.nextInt();
            int c=0,sum=0;
            arr=new int[n];
            for(int i=0;i<n;i++)
            {
                arr[i]=sc.nextInt();
                if(isprime(arr[i]))
                {
                    c++;
                    sum=sum+arr[i];
                }
                
            }
            System.out.format("%.2f",(float)sum/c);
        }
        
    }

---
+++ Average_of_primes.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class Main
+{
+    public static boolean isprime(int n)
+    {
+    int count=0;
+    if(n==1)
+    return false;
+    for(int i=2;i<=(int)Math.sqrt(n);i++)
+    {
+        if(n%i==0)
+        count++;
+    }
+    if(count==0)
+    return true;
+    else
+    return false;
+
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        int arr[];
+        n=sc.nextInt();
+        int c=0,sum=0;
+        arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            if(isprime(arr[i]))
+            {
+                c++;
+                sum=sum+arr[i];
+            }
+            
+        }
+        System.out.format("%.2f",(float)sum/c);
+    }
+    
+}
