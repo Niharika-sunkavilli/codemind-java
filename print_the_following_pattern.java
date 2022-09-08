7c115ea066610972faa08c83d6b6f97837efbfd1
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-09-08 21:15:48 +0530

    import java.util.Scanner; class Codemind {   public static void main(String args[])   {     Scanner sc=new Scanner(System.in);     int n;     n=sc.nextInt(); 	for(int i=n;i>=1;i--) 	{ 	  for(int j=i;j>=1;j--) 		{ 		  System.out.print((char)(64+i)+" "); 		} 		System.out.println(); 	}   } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
-class Main
+class Codemind
 {
-    public static void main(String args[])
-    {
-        int n;
-        Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=i;j++)
-            {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
-    }
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n;
+    n=sc.nextInt();
+	for(int i=n;i>=1;i--)
+	{
+	  for(int j=i;j>=1;j--)
+		{
+		  System.out.print((char)(64+i)+" ");
+		}
+		System.out.println();
+	}
+  }
 }
