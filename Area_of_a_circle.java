8761a6faa8ff0f9cf921cfa5166a98fc7034f1b2
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-09-13 21:39:07 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         int r;         Scanner sc=new Scanner(System.in);         r=sc.nextInt();         float area;         area=(float)3.14*r*r;         System.out.format("%.2f",area);     } }

--- Area_of_a_circle.java
+++ Area_of_a_circle.java
@@ -3,10 +3,10 @@ class Main
 {
     public static void main(String args[])
     {
-        Scanner sc =new Scanner(System.in);
         int r;
-        float area;
+        Scanner sc=new Scanner(System.in);
         r=sc.nextInt();
+        float area;
         area=(float)3.14*r*r;
         System.out.format("%.2f",area);
     }
