b6fe8d9e6f47c9f933409c14c82fdf8ecded9258
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-09-15 21:55:51 +0530

    import java.util.Scanner; class Main {     public static boolean isprime(int n)     {         if(n==1)         return false;         int c=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             c++;         }         if(c==0)         return true;         else         return false;     }     public static void main(String args[])     {         int n,c=0,rem,l;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         l=(int)Math.log10(n)+1;         if(isprime(n))//53         {             while(n>0)             {             rem=n%10;             if(isprime(rem))             {                 c++;             }             n=n/10;             }         }         if(c==l)         System.out.println("Mega Prime");         else         System.out.println("Not Mega Prime");     } }

--- mega_prime.java
+++ mega_prime.java
@@ -3,9 +3,9 @@ class Main
 {
     public static boolean isprime(int n)
     {
-        int c=0;
         if(n==1)
         return false;
+        int c=0;
         for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
             if(n%i==0)
@@ -18,29 +18,25 @@ class Main
     }
     public static void main(String args[])
     {
+        int n,c=0,rem,l;
         Scanner sc=new Scanner(System.in);
-        int n,c=0,d;
         n=sc.nextInt();
-        int rem;
-        d=(int)Math.log10(n)+1;
-        if(isprime(n))
+        l=(int)Math.log10(n)+1;
+        if(isprime(n))//53
         {
-        while(n>0)
-        {
+            while(n>0)
+            {
             rem=n%10;
-            n=n/10;
             if(isprime(rem))
-            c++;
-            else
-            continue;
-            
+            {
+                c++;
+            }
+            n=n/10;
+            }
         }
-        if(c==d)
+        if(c==l)
         System.out.println("Mega Prime");
         else
         System.out.println("Not Mega Prime");
-        }
-        else
-	System.out.println("Not Mega Prime");
     }
 }
