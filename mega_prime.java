22da92d1e9981f3a4d75a96ae1000a1f3bd23b27
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-09-16 20:38:34 +0530

    import java.util.Scanner; class Main {     public static boolean prime(int n)     {         int count=0;         if(n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             count++;
            }
            if(count==0)
            return true;
            else
            return false;
        }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n,rem,c=0,d;
            n=sc.nextInt();
            d=(int)Math.log10(n)+1;
            if(prime(n))
            {
            while(n>0)
            {
                rem=n%10;
                n=n/10;
                if(prime(rem))
                c++;
            }
            if(c==d)
            System.out.println("Mega Prime");
            else
            System.out.println("Not Mega Prime");
            
            }
            else
            System.out.println("Not Mega Prime");
        }
    }

--- mega_prime.java
+++ mega_prime.java
@@ -1,41 +1,43 @@
 import java.util.Scanner;
 class Main
 {
-    public static boolean isprime(int n)
+    public static boolean prime(int n)
     {
+        int count=0;
         if(n==1)
         return false;
-        int c=0;
         for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
             if(n%i==0)
-            c++;
+            count++;
+            
         }
-        if(c==0)
+        if(count==0)
         return true;
         else
         return false;
     }
     public static void main(String args[])
     {
-        int n,c=0,rem,l;
         Scanner sc=new Scanner(System.in);
+        int n,rem,c=0,d;
         n=sc.nextInt();
-        l=(int)Math.log10(n)+1;
-        if(isprime(n))//53
+        d=(int)Math.log10(n)+1;
+        if(prime(n))
         {
-            while(n>0)
-            {
+        while(n>0)
+        {
             rem=n%10;
-            if(isprime(rem))
-            {
-                c++;
-            }
             n=n/10;
-            }
+            if(prime(rem))
+            c++;
         }
-        if(c==l)
+        if(c==d)
         System.out.println("Mega Prime");
+        else
+        System.out.println("Not Mega Prime");
+        
+        }
         else
         System.out.println("Not Mega Prime");
     }
