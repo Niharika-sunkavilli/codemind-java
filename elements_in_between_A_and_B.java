bd57b6162b0ca3658c6a215cd2732c946a04ef44
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-09-17 20:21:02 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[],c=0;         n=sc.nextInt();         arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int a=sc.nextInt();         int b=sc.nextInt();         for(int i=0;i<n;i++)         {             if(arr[i]>=a && arr[i]<=b)             {                 System.out.print(arr[i]+" ");                 c++;             }
            }
            
            if(c==0)
            System.out.println("-1");
        }
    }

---
+++ elements_in_between_A_and_B.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,arr[],c=0;
+        n=sc.nextInt();
+        arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>=a && arr[i]<=b)
+            {
+                System.out.print(arr[i]+" ");
+                c++;
+            }
+            
+        }
+        
+        if(c==0)
+        System.out.println("-1");
+    }
+}
