9ba5db432dc3a32961aa5b2a0214958088eb222d
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-09-22 13:58:25 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[];         int k=0;         n=sc.nextInt();         arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();             if(arr[i]%2!=0)             {                 k=i;             }         }         System.out.println(k);     } }

--- last_odd_number_index_position.java
+++ last_odd_number_index_position.java
@@ -4,20 +4,18 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,arr[],x=0;
+        int n,arr[];
+        int k=0;
         n=sc.nextInt();
         arr=new int[n];
         for(int i=0;i<n;i++)
         {
             arr[i]=sc.nextInt();
-            
-        }
-       
-        for(int i=0;i<n;i++)
-        {
             if(arr[i]%2!=0)
-            x=i;
+            {
+                k=i;
+            }
         }
-        System.out.println(x);
+        System.out.println(k);
     }
 }
