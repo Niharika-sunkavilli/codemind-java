581feef1443a0cd637e20ef3cfeb0030433d6d7a
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-09-24 22:57:49 +0530

    import java.util.Scanner; class Lcm {     public static void main(String args[])     {         int a,b,max,lcm;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         if(a>=b)             max=a;         else            max=b;         for(int i=max; ;i++)         {             if(i%a==0 && i%b==0)              {               lcm=i;               break;              }         }         System.out.println(lcm);         sc.close();     } }

---
+++ LCM.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Lcm
+{
+    public static void main(String args[])
+    {
+        int a,b,max,lcm;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        if(a>=b)
+            max=a;
+        else
+           max=b;
+        for(int i=max; ;i++)
+        {
+            if(i%a==0 && i%b==0)
+             {
+              lcm=i;
+              break;
+             }
+        }
+        System.out.println(lcm);
+        sc.close();
+    }
+}
