10be9a2e354a59a1fbd87139dfc86a1310b7b39b
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-07 14:11:41 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[],a,b,c=0;         n=sc.nextInt();         arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         a=sc.nextInt();         b=sc.nextInt();         for(int i=0;i<n;i++)         {             if(arr[i]<a || arr[i]>b)             {             System.out.print(arr[i]+" ");             c++;             }         }         if(c==0)         System.out.println(-1);     } }

--- elements_not_in_between_A_and_B.java
+++ elements_not_in_between_A_and_B.java
@@ -4,26 +4,22 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,arr[];
+        int n,arr[],a,b,c=0;
         n=sc.nextInt();
         arr=new int[n];
         for(int i=0;i<n;i++)
         {
             arr[i]=sc.nextInt();
         }
-        int a=sc.nextInt();
-        int c=0;
-        int b=sc.nextInt();
+        a=sc.nextInt();
+        b=sc.nextInt();
         for(int i=0;i<n;i++)
         {
-            if(arr[i]>=a && arr[i]<=b)
-            continue;
-            else
+            if(arr[i]<a || arr[i]>b)
             {
             System.out.print(arr[i]+" ");
             c++;
             }
-        
         }
         if(c==0)
         System.out.println(-1);
