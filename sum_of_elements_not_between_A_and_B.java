4c824ff24d45e5f96b40f292065adeb7c99165e0
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-07 14:00:51 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[],a,b,sum=0;         n=sc.nextInt();         arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         a=sc.nextInt();         b=sc.nextInt();         for(int i=0;i<n;i++)         {             if(arr[i]<a || arr[i]>b)             sum+=arr[i];         }         System.out.println(sum);     } }

--- sum_of_elements_not_between_A_and_B.java
+++ sum_of_elements_not_between_A_and_B.java
@@ -4,28 +4,20 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,arr[];
+        int n,arr[],a,b,sum=0;
         n=sc.nextInt();
         arr=new int[n];
         for(int i=0;i<n;i++)
         {
             arr[i]=sc.nextInt();
         }
-        int a=sc.nextInt();
-        int sum=0;
-        int b=sc.nextInt();
+        a=sc.nextInt();
+        b=sc.nextInt();
         for(int i=0;i<n;i++)
         {
-            if(arr[i]>=a && arr[i]<=b)
-            continue;
-            else
-            {
+            if(arr[i]<a || arr[i]>b)
             sum+=arr[i];
-            
-            }
-        
         }
-        
         System.out.println(sum);
     }
 }
