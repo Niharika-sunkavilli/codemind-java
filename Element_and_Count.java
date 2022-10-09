c4dc8d7e5be131701fb9a45d0879900433364741
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-09 12:39:42 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[],c=0;         n=sc.nextInt();         arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             c=1;             if(arr[i]!=-99)             {                 for(int j=0;j<n;j++)                 {                     if(arr[i]==arr[j] && i!=j)                     {                         c++;                         arr[j]=-99;                     }                 }                 System.out.print(arr[i]+" "+c+" ");             }         }     } }

---
+++ Element_and_Count.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,arr[],c=0;
+        n=sc.nextInt();
+        arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            c=1;
+            if(arr[i]!=-99)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(arr[i]==arr[j] && i!=j)
+                    {
+                        c++;
+                        arr[j]=-99;
+                    }
+                }
+                System.out.print(arr[i]+" "+c+" ");
+            }
+        }
+    }
+}
