fc9ff361a51c673ca7c865c77bf04eb2df1a00ef
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-09 12:10:36 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[];         n=sc.nextInt();         arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {
                if(arr[i]!=-99)
                {
                    for(int j=0;j<n;j++)
                    {
                        if(arr[i]==arr[j] && i!=j)
                        {
                            arr[j]=-99;
                        }
                    }
                    System.out.print(arr[i]+" ");
                }
                
            }
        }
    }

--- unique_elements.java
+++ unique_elements.java
@@ -4,7 +4,7 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,arr[],c=0;
+        int n,arr[];
         n=sc.nextInt();
         arr=new int[n];
         for(int i=0;i<n;i++)
@@ -13,19 +13,19 @@ class Main
         }
         for(int i=0;i<n;i++)
         {
-            c=1;
+            
             if(arr[i]!=-99)
             {
                 for(int j=0;j<n;j++)
                 {
-                   if(arr[i]==arr[j] && i!=j)
-                   {
-                       c++;
-                       arr[j]=-99;
-                   }
+                    if(arr[i]==arr[j] && i!=j)
+                    {
+                        arr[j]=-99;
+                    }
                 }
                 System.out.print(arr[i]+" ");
             }
+            
         }
     }
 }
