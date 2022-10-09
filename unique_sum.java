183051ae4fd03ef83f42825c63c4d2f5fed993f5
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-09 12:21:27 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[],sum=0;         n=sc.nextInt();         arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(arr[i]!=-99)             {                 for(int j=0;j<n;j++)                 {                     if(arr[i]==arr[j] && i!=j)                     {                         arr[j]=-99;                     }
                    }
                    sum+=arr[i];
                }
                
            }
            System.out.println(sum);
        }
    }

---
+++ unique_sum.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,arr[],sum=0;
+        n=sc.nextInt();
+        arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]!=-99)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(arr[i]==arr[j] && i!=j)
+                    {
+                        arr[j]=-99;
+                    }
+                    
+                }
+                sum+=arr[i];
+            }
+            
+        }
+        System.out.println(sum);
+    }
+}
