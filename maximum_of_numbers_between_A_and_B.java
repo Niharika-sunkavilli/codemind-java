caf51cada58b05bb5d851061c9a8aba2c08017f4
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-10 15:03:14 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[],a,b,max=0,c=0;         n=sc.nextInt();         arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         a=sc.nextInt();         b=sc.nextInt();         for(int i=0;i<n;i++)         {             if(arr[i]>=a && arr[i]<=b)             {                 if(arr[i]>max)                 max=arr[i];                 c++;
                }
            }
            if(c>0)
            System.out.println(max);
            else
            System.out.println(-1);
        }
    }

--- maximum_of_numbers_between_A_and_B.java
+++ maximum_of_numbers_between_A_and_B.java
@@ -4,28 +4,28 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,arr[],c=0;
+        int n,arr[],a,b,max=0,c=0;
         n=sc.nextInt();
         arr=new int[n];
         for(int i=0;i<n;i++)
         {
             arr[i]=sc.nextInt();
         }
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int max=0;
+        a=sc.nextInt();
+        b=sc.nextInt();
         for(int i=0;i<n;i++)
         {
             if(arr[i]>=a && arr[i]<=b)
             {
-            if(arr[i]>max)
-            max=arr[i];
-            c++;
+                if(arr[i]>max)
+                max=arr[i];
+                c++;
+                
             }
         }
-        if(c==0)
-        System.out.println(-1);
+        if(c>0)
+        System.out.println(max);
         else
-         System.out.println(max);
+        System.out.println(-1);
     }
 }
