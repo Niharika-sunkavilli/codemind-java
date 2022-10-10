fe2561bc81fdc80ed1bea8e279e876776a82e68a
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-10 14:37:42 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[],c=0,f=0;         n=sc.nextInt();         arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(arr[i]%2==0)             c++;             if(arr[i]%2==0 && i%2==0)             f++;         }         if(c==f)         System.out.println("True");         else         System.out.println("False");     } }

---
+++ Strictly_EVEN.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,arr[],c=0,f=0;
+        n=sc.nextInt();
+        arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]%2==0)
+            c++;
+            if(arr[i]%2==0 && i%2==0)
+            f++;
+        }
+        if(c==f)
+        System.out.println("True");
+        else
+        System.out.println("False");
+    }
+}
