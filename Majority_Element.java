bf2761a9bac1e0ee661e584e95bcc6b907521caf
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-23 14:24:02 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[],c;         n=sc.nextInt();         arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             c=1;             if(arr[i]!=-99)             {                 for(int j=0;j<n;j++)                 {                     if(arr[i]==arr[j] && i!=j)                     {                         c++;                         arr[j]=-99;                     }                 }                 if(c>n/2)                 System.out.println(arr[i]);             }         }     } }

---
+++ Majority_Element.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,arr[],c;
+        n=sc.nextInt();
+        arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            c=1;
+            if(arr[i]!=-99)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(arr[i]==arr[j] && i!=j)
+                    {
+                        c++;
+                        arr[j]=-99;
+                    }
+                }
+                if(c>n/2)
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
