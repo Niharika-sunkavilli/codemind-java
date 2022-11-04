336a68624ecff81db92f6b9f3173f0f4a69e0c84
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-11-04 22:08:12 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int c=0;         for(int i=0;i<s.length();i++)         {             char ch=s.charAt(i);             if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')             c++;         }         System.out.println(c);     } }

---
+++ write_a_program_to_get_the_number_of_vowels_in_the_input_string.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+            c++;
+        }
+        System.out.println(c);
+    }
+}
