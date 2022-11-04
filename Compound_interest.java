349b2f30838bc519b0d9decb8c086ef5fef5d0de
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-11-04 18:50:56 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double p,t,r;         double si;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         si=p*(Math.pow((1+t/100),r));         System.out.format("%.2f",si);     } }

--- Compound_interest.java
+++ Compound_interest.java
@@ -5,11 +5,11 @@ class Main
     {
         Scanner sc=new Scanner(System.in);
         double p,t,r;
+        double si;
         p=sc.nextInt();
         t=sc.nextInt();
         r=sc.nextInt();
-        double ci;
-        ci=p*(Math.pow((1+t/100),r));
-        System.out.format("%.2f",ci);
+        si=p*(Math.pow((1+t/100),r));
+        System.out.format("%.2f",si);
     }
 }
