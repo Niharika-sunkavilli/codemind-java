d0375c8f91e6dd8b7b91079990cc89080feb4caa
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-11-05 21:53:11 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         char max='a';         for(int i=0;i<s.length();i++)         {             char ch=s.charAt(i);             if(ch>max)             max=ch;         }         System.out.println(max);     } }

---
+++ max_of_string.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        char max='a';
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch>max)
+            max=ch;
+        }
+        System.out.println(max);
+    }
+}
