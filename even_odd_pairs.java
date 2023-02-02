6c407df3db504100b1d74d945338d2f75a2597fe
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2023-02-02 22:56:04 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int e[]=new int[n];         int o[]=new int[n];         int j=0,k=0;         for(int i=0;i<n;i++)         {             if(arr[i]%2==0)             e[j++]=arr[i];             else             o[k++]=arr[i];         }         int min=Math.min(j,k);         for(int i=0;i<min;i++)         {             System.out.print(e[i]+" "+o[i]+" ");         }         if(min==j)         {             for(int i=min;i<k;i++)             System.out.print(o[i]+" ");         }         else         {             for(int i=min;i<j;i++)             {                 System.out.print(e[i]+" ");             }         }         if(n%2!=0)         System.out.print("0");     } }

---
+++ even_odd_pairs.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int e[]=new int[n];
+        int o[]=new int[n];
+        int j=0,k=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]%2==0)
+            e[j++]=arr[i];
+            else
+            o[k++]=arr[i];
+        }
+        int min=Math.min(j,k);
+        for(int i=0;i<min;i++)
+        {
+            System.out.print(e[i]+" "+o[i]+" ");
+        }
+        if(min==j)
+        {
+            for(int i=min;i<k;i++)
+            System.out.print(o[i]+" ");
+        }
+        else
+        {
+            for(int i=min;i<j;i++)
+            {
+                System.out.print(e[i]+" ");
+            }
+        }
+        if(n%2!=0)
+        System.out.print("0");
+    }
+}
